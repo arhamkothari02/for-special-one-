2bc4c2475f9ea763c2094337085e5c6ee3e6bedf
arhamkothari02 <arham.kothari02@gmail.com>
2026-07-22 12:22:14 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
+const btn = document.getElementById("startBtn");
+const status = document.getElementById("status");
 
+btn.addEventListener("click", () => {
+
+const username =
+document.getElementById("username").value.trim().toLowerCase();
+
+const password =
+document.getElementById("password").value.trim().toLowerCase();
+
+if(
+username === "rashi belsare" &&
+password === "bauni belsare"
+){
+
+status.style.color="#33e0ff";
+status.innerHTML="INITIALIZING AUTHENTICATION...";
+
+setTimeout(()=>{
+
+window.location.href="page2.html";
+
+},2000);
+
+}else{
+
+status.style.color="#ff4d4d";
+status.innerHTML="AUTHENTICATION FAILED";
+
+}
+
+});
